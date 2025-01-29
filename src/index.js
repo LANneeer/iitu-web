83166da54a79920a132fd05e6d40ef7f153feefb
Temirlan Ilik <0penguin@mail.ru>
2025-01-29 12:25:02 +0500

    step-8

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import TodoList from "./components/todo-list";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
+  const todoData = [
+    { label: "Drink Coffee", important: false },
+    { label: "Implement React Application", important: true },
+    { label: "Make notes from your stud", important: false },
+    { label: "Do not drink alchohol", important: false },
+  ];
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData} />
     </div>
   );
 };
-
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 root.render(<App />);
