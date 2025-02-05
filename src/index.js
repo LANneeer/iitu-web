253f81c894115d84625214d45ce8716802db99e4
Temirlan Ilik <0penguin@mail.ru>
2025-02-05 12:43:14 +0500

    step-17

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from "react";
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
+import App from "./components/app";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Implement React Application", important: true, id: 2 },
-    { label: "Make notes from your stud", important: false, id: 3 },
-    { label: "Do not drink alchohol", important: false, id: 4 },
-  ];
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData} />
-      <ItemStatusFilter />
-    </div>
-  );
-};
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 root.render(<App />);
